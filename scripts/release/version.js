c9af7db0fbe496a084959dbcf494d8d61c1d6d6e
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-06-13 14:16:08 -0300

    chore: create version.js

---
+++ scripts/release/version.js
@@ -0,0 +1,9 @@
+import spawn from "cross-spawn";
+
+spawn.sync("npx", ["changeset", "version"], { stdio: "inherit" });
+
+spawn.sync(
+  "npm",
+  ["i", "--package-lock-only", "--no-audit", "--ignore-scripts", "--no-fund"],
+  { stdio: "inherit" }
+);
